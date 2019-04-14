36ff3d1f12c7751b3307c80cdbe30ca1ba15c775
Thomas Baumer <thomas.baumer@stud.uni-regensburg.de>
2019-04-14 22:57:37 +0200

    Version 14.04. Mail Flo

--- reporting/include/reporting.hpp
+++ reporting/include/reporting.hpp
@@ -10,27 +10,36 @@ CONTRACT reporting : public contract {
   
     ACTION init();
     ACTION enrol(name user);
-    ACTION appoint(name user);
-    ACTION report( name reporter, std::string hash, uint64_t parentLink, bool isIncident );
+    ACTION report(name reporter, std::string hash, uint64_t parentLink, bool isIncident);
     ACTION approve(uint64_t key);
     ACTION vote(uint64_t itemKey, name voter, uint64_t merit);
+    ACTION transfer(name from, name to, uint64_t amount);
+    ACTION blame(name blamer, name blamed, std::string reason, bool freeze);
+    ACTION voteb(uint64_t blameKey, name voter, bool value);
+    ACTION buy(name buyer, uint64_t itemKey);
+    ACTION received(name buyer, uint64_t itemKey);
   
   private:
   
+    void appoint(name user);
+	  
 	  bool initialized = false;
 	  
     uint64_t statusThreshold = 10;
 	  uint64_t voteThreshold = 2;
+	  uint64_t blameThreshold = 1;
 	  uint64_t minConfirmations = 1;
+	  uint64_t minConfirmationsBlame = 2;
 	  uint64_t conversionFactor = 10;
     
     TABLE user {
-  		name			user;
-  		uint64_t		balance;
-  		uint64_t 		statusR;
-  		uint64_t 		statusV;
-  		bool 			  verificator;
-  		bool			  frozen;
+  		name			    user;
+  		uint64_t		  balance;
+  		uint64_t 		  statusR;
+  		uint64_t 		  statusV;
+  		uint64_t      blames;
+  		bool 			    verificator;
+  		bool			    frozen;
   		uint64_t primary_key() const { return user.value; }
     };
     typedef eosio::multi_index<"users"_n, user> user_t;
@@ -51,13 +60,44 @@ CONTRACT reporting : public contract {
 	  typedef eosio::multi_index<"item"_n, item> item_t;
 	  
 	  TABLE voting {
-  		uint64_t 		key;
-  		uint64_t		itemKey;
-  		name			  voter;
-  		uint64_t		value;
+  		uint64_t 		  key;
+  		uint64_t		  itemKey;
+  		name			    voter;
+  		uint64_t		  value;
 		  uint64_t primary_key() const { return key; }
 	  };
 	  typedef eosio::multi_index<"voting"_n, voting> voting_t;
+
+  	TABLE blaming {
+  		uint64_t		  key;
+  		name			    blamer;
+  		name			    blamed;
+  		std::string	  reason;
+  		bool          freeze;
+  		bool			    voteable;
+  		uint64_t 		  confirmations;
+  		uint64_t 		  votes;
+  		uint64_t primary_key() const { return key; }
+  	};
+  	typedef eosio::multi_index<"blaming"_n, blaming> blaming_t;
+  	
+  	TABLE votingb {
+  		uint64_t 		  key;
+  		uint64_t		  blameKey;
+  		name			    voter;
+  		bool		      value;
+  		uint64_t primary_key() const { return key; }
+  	};
+  	typedef eosio::multi_index<"votingb"_n, votingb> votingb_t;
+  	
+  	TABLE order {
+  		uint64_t 	    key;
+  		uint64_t	    itemKey;
+  		name			    buyer;
+  		bool			    received;
+  		uint64_t primary_key() const { return key; }
+  	};
+  	typedef eosio::multi_index<"order"_n, order> order_t;
 };
 
-EOSIO_DISPATCH(reporting, (init)(enrol))
+EOSIO_DISPATCH(reporting, (init) (enrol) (report)(approve)(vote) (transfer)(buy)(received) (blame)(voteb))
--- reporting/src/reporting.cpp
+++ reporting/src/reporting.cpp
@@ -4,35 +4,49 @@ ACTION reporting::init() {
 		require_auth( _self );
 		check ( !(initialized), "Initialization already done.");
 		user_t users( _self, _self.value );
-		users.emplace(_self, [&]( auto& row ) { row.user = "oone"_n;  row.balance = 10;  row.statusR = 10;  row.statusV = 0;  row.verificator = 1;  row.frozen = 0; });
-		users.emplace(_self, [&]( auto& row ) { row.user = "delekom"_n;  row.balance = 10;  row.statusR = 10;  row.statusV = 0;  row.verificator = 0;  row.frozen = 0; });
+		users.emplace(_self, [&]( auto& row ) { row.user = "oone"_n; row.balance = 10; row.statusR = 10; row.statusV = 0; row.blames = 0; row.verificator = 1; row.frozen = 0; });
+		users.emplace(_self, [&]( auto& row ) { row.user = "delekom"_n; row.balance = 10;row.statusR = 10; row.statusV = 0; row.blames = 0; row.verificator = 1; row.frozen = 0; });
+		users.emplace(_self, [&]( auto& row ) { row.user = "bzz"_n; row.balance = 10; row.statusR = 7; row.statusV = 0; row.blames = 0; row.verificator = 0; row.frozen = 0; });
 		item_t item( _self, _self.value );
 		item.emplace(_self, [&]( auto& row ) { row.key = 0;  row.parentLink = 0;  row.reporter = _self;  row.hash = "The Russians.";  row.incident = 1; row.voteable = 0;  row.approval = 0;  row.confirmations = 0;  row.votes = 0;  row.rating = 0; });
-		voting_t voting( _self, _self.value );
-		voting.emplace(_self, [&]( auto& row ) { row.key = 0; row.itemKey = 0; row.voter = _self; row.value = 0; });
 		initialized = true;
 }
+
+
+
+
+
 
 ACTION reporting::enrol(name user) {
   require_auth(user);
   user_t users( _self, _self.value );
-	auto iterator = users.find(user.value);
-	if (iterator == users.end()) { users.emplace(_self, [&]( auto& row ) { row.user = user; row.balance = 0; row.statusR = 0; row.statusV = 0; row.verificator = 0; row.frozen = 0; }); }
-	else { print("User already on the blockchain." ); }
+	auto it = users.find(user.value);
+	check(it == users.end(), "User already on the blockchain.");
+	users.emplace(_self, [&]( auto& row ) { row.user = user; row.balance = 0; row.statusR = 0; row.statusV = 0; row.blames = 0; row.verificator = 0; row.frozen = 0; });
 }
 
-ACTION reporting::appoint(name user) {
-	require_auth( user );
+void reporting::appoint(name user) {
 	user_t users( _self, _self.value );
 	auto iterator = users.find(user.value);
 	check( !(iterator == users.end()), "No such user on the blockchain.");
-	check( !(iterator->frozen), "Your account is frozen.");
-	check( iterator->statusR >= statusThreshold, "Not enough status points earned to become a verificator.");
+	check( !(iterator->frozen), "This user is frozen.");
+	check( iterator->statusR >= statusThreshold, "The user did not earn enough status points to become a verificator.");
 	users.modify(iterator, _self, [&]( auto& row ) { row.verificator = 1; });
 }
 
+
+
+
+
+
+
 ACTION reporting::report(name reporter, std::string hash, uint64_t parentLink, bool isIncident) {
 	require_auth( reporter );
+	user_t users( _self, _self.value );
+	auto it_reporter = users.find(reporter.value);
+	check( !(it_reporter == users.end()), "No such user on the blockchain.");
+	check( !(it_reporter->frozen), "This user is frozen.");
+	
 	item_t item( _self, _self.value );
 	for(auto& row : item) { check( !(row.hash == hash), "That item was already uploaded." ); }
 	item.emplace(_self, [&]( auto& row ) { row.key = item.available_primary_key(); row.parentLink = parentLink; row.reporter = reporter; row.hash = hash; row.incident = isIncident; row.voteable = 1;  row.approval = 0;  row.confirmations = 0;  row.votes = 0;  row.rating = 0; });
@@ -53,13 +67,13 @@ ACTION reporting::vote(uint64_t itemKey, name voter, uint64_t merit) {
 	check( !(it_voter->frozen) , "Your account is frozen." );
 	check( it_voter->verificator, "You have no voting permission." );
 
-	voting_t voting( _self, _self.value );
-	for(auto& row : voting) { check( !( row.itemKey == itemKey && row.voter == voter ), "You already voted for that item." ); }
-
 	item_t item( _self, _self.value );
 	auto it_item = item.find(itemKey);
 	check( it_item->voteable, "You cannot vote for that." );
 	check( !(it_item->reporter == voter), "You cannot vote for your own item" );
+
+	voting_t voting( _self, _self.value );
+	for(auto& row : voting) { check( !( row.itemKey == itemKey && row.voter == voter ), "You already voted for that item." ); }
 	
 	bool voteConfirms = 1; if(merit < 1) { voteConfirms = 0; }
 	bool isVoteable = 1; if (it_item->votes + 1 >= voteThreshold) { isVoteable = 0; }
@@ -68,4 +82,122 @@ ACTION reporting::vote(uint64_t itemKey, name voter, uint64_t merit) {
 	users.modify(it_voter, _self, [&]( auto& row ) { row.statusV = row.statusV + 1; });
 	voting.emplace(_self, [&]( auto& row ) { row.key = voting.available_primary_key(); row.itemKey = itemKey; row.voter = voter; row.value = merit; });
 	item.modify(it_item, _self, [&]( auto& row ) { row.confirmations = row.confirmations + voteConfirms; row.votes = row.votes + 1; row.rating = row.rating + merit; row.voteable = isVoteable; });
+	if (!isVoteable && it_item->confirmations < minConfirmations) { item.modify(it_item, _self, [&]( auto& row ) { row.rating = 0; }); }
+	//appoint as verificator if enough status points are earned
+	if(it_reporter->statusR >= statusThreshold) { reporting::appoint( it_reporter->user); }
 }
+
+
+
+
+
+
+
+
+ACTION reporting::transfer(name from, name to, uint64_t amount) {
+	require_auth( from );
+	user_t users( _self, _self.value ); 
+	auto it_from = users.find(from.value); 
+	auto it_to = users.find(to.value);
+	
+	check( !(it_from == users.end()), "The sending account does not exist. Please enrol.");
+	check( !(it_to == users.end()), "The receiving account does not exist. Please enrol.");
+  check( !(it_from->frozen) , "The sending account is frozen." );
+  check( !(it_to->frozen) , "The receiving account is frozen." );
+	check( it_from->balance - amount < it_from->balance , "IntegerOverflow (sender).");
+	check( it_to->balance + amount > it_to->balance , "IntegerOverflow (receiver).");
+	check( it_from->balance - amount >= 0 , "Sender has not enough token.");
+	check( !(it_from == it_to), "You have to send the token to another user.");
+
+	users.modify(it_from, _self, [&]( auto& row ) { row.balance = row.balance - amount; });
+	users.modify(it_to, _self, [&]( auto& row ) { row.balance = row.balance + amount; });
+}
+
+ACTION reporting::buy( name buyer, uint64_t itemKey ) {
+	require_auth( buyer );
+	user_t users( _self, _self.value );
+	auto it_buyer = users.find(buyer.value);
+	check( !(it_buyer == users.end()) , "Please enrol first." );
+	check( !(it_buyer->frozen) , "Your account is frozen." );
+
+	item_t item( _self, _self.value );
+	auto it_item = item.find(itemKey);
+	check( !(it_item == item.end()), "No such item" );
+	check( !(it_item->voteable), "Voting for this item is not yet done." );
+	check( !(it_item->reporter == buyer), "You cannot buy for your own item." );
+	
+	name seller = it_item->reporter;
+	uint64_t price = it_item->rating/conversionFactor;
+	order_t order( _self, _self.value );
+	for(auto& row : order) { check( !(row.itemKey == itemKey && row.buyer == buyer), "That order was already placed." ); }
+	order.emplace(_self, [&]( auto& row ) { row.key = order.available_primary_key();  row.itemKey = itemKey; row.buyer = buyer; row.received = 0; });
+	reporting::transfer(buyer, seller, price);
+}
+
+ACTION reporting::received( name buyer, uint64_t itemKey, bool done ) {
+	require_auth( buyer );
+	user_t users( _self, _self.value );
+	auto it_buyer = users.find(buyer.value);
+	check( !(it_buyer == users.end()) , "Please enrol first." );
+	check( !(it_buyer->frozen) , "Your account is frozen." );
+
+	order_t order( _self, _self.value );
+	auto it_order = order.find(itemKey);
+	check( !(it_order == order.end()), "No such order." );
+	check( !(it_order->buyer == buyer ), "That's not your order." );
+	if (done) { order.modify(it_order, _self, [&]( auto& row ) { row.received = 1; }); }
+	else { 
+	  item_t item( _self, _self.value );
+	  auto it_item = item.find(it_order->itemKey);
+	  reporting::blame(buyer, it_item->reporter, "Order " + it_order->itemKey + " not received.", 1);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ACTION reporting::blame(name blamer, name blamed, std::string reason, bool freeze) {
+	require_auth( blamer );
+	user_t users( _self, _self.value );
+	auto it_blamer = users.find(blamer.value);
+	check( !(it_blamer == users.end()) , "Please enrol first." );
+	check( !(it_blamer->frozen) , "Your account is frozen." );
+	
+	blaming_t blaming( _self, _self.value );
+	blaming.emplace( _self, [&]( auto& row ) { row.key = blaming.available_primary_key(); row.blamer = blamer;  row.blamed = blamed; row.reason = reason; row.freeze = freeze; row.voteable = 1; row.confirmations = 0; row.votes = 0; });
+}
+
+ACTION reporting::voteb(uint64_t blameKey, name voter, bool value) {
+	require_auth( voter );
+	user_t users( _self, _self.value );
+	auto it_voter = users.find(voter.value);
+	check( !(it_voter->frozen) , "Your account is frozen." );
+	check( it_voter->verificator, "You have no voting permission." );
+
+	blaming_t blaming( _self, _self.value );
+	auto it_blaming = blaming.find(blameKey);
+	check( it_blaming->voteable, "You cannot vote for that." );
+	check( !(it_blaming->blamer == voter), "You cannot vote for your own blaming" );
+
+	votingb_t votingb( _self, _self.value );
+	for(auto& row : votingb) { check( !( row.blameKey == blameKey && row.voter == voter ), "You already voted for that item." ); }	
+	
+	bool isVoteable = 1; if (it_blaming->votes + 1 >= voteThreshold) { isVoteable = 0; }
+	votingb.emplace(_self, [&]( auto& row ) { row.key = votingb.available_primary_key(); row.blameKey = blameKey; row.voter = voter; row.value = value; });
+	blaming.modify(it_blaming, _self, [&]( auto& row ) { row.confirmations = row.confirmations + value; row.votes = row.votes + 1; row.voteable = isVoteable; });
+	if (!isVoteable && it_blaming->confirmations >= minConfirmationsBlame) { 
+    auto it_blamed = users.find(it_blaming->blamed.value);
+  	users.modify(it_blamed, _self, [&]( auto& row ) { row.blames = row.blames + it_blaming->freeze; });
+  	if (it_blamed->blames >= blameThreshold) { users.modify(it_blamed, _self, [&]( auto& row ) { row.frozen = it_blaming->freeze; }); }
+	}
+}
+
+
